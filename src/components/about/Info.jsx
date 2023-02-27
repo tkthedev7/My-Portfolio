3268f82af40c693549d1870e4b5901f0b303ebc3
Tristan Klinski <tristanklinski@gmail.com>
2023-02-27 16:24:49 -0500

    continuing about/info section

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className="about__info grid">
+      <div className="about__box">
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">Over four years of professional IT experience.</span>
+      </div>
+
+      <div className="about__box">
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">Several projects involving DevOps, Software Development, and Cloud Architecture.</span>
+      </div>
+
+      <div className="about__box">
+        <h3 className="about__title">Soft Skills</h3>
+        <span className="about__subtitle"></span>
+      </div>
+    </div>
   )
 }
 
