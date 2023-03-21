14e9f5217ca6f33e7cd221f68c1a2adceba6e328
Tristan <tklinski@oxya.com>
2023-03-21 13:50:19 -0400

    fixed mobile formatting for contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "./contact.css";
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_0afiwht', 'template_p58021f', form.current, '4DUSvwMcJSVDtLBpm')
+        e.target.reset()
+        };
+
   return (
     <section className="contact section" id="contact">
         <h2 className="section__title">Contact</h2>
@@ -47,7 +57,7 @@ const Contact = () => {
             <div className="contact__content">
                 <h3 className="contact__title">Send an Email</h3>
 
-                <form className="contact__form">
+                <form ref={form} onSubmit={sendEmail} className="contact__form">
                     <div className="contact__form-div">
                         <label className="contact__form-tag">Name</label>
                         <input type="text" name="name" 
